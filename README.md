7662d2199322da5e3098df066d90e4f26d1031a2
nbashaw <nbashaw@gmail.com>
2019-03-19 18:46:54 -0700

    first cut

---
+++ README.md
@@ -0,0 +1,8 @@
+# Simple Substack Importer
+
+Hastily-written instructions: 
+
+1. Fork and clone this locally
+3. Install dependencies (`npm i`)
+4. Create a `.env` file with `SUBSTACK_IMPORT_KEY` and `SUBSTACK_SUBDOMAIN`. (If you need an import key email support@substack.com.)
+5. Deploy it to Heroku and set the env vars.
---
+++ app.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { createHash } = require('crypto');
+const rp = require('request-promise-native');
+const substack_key = process.env.SUBSTACK_IMPORT_KEY;
+const subdomain = process.env.SUBSTACK_SUBDOMAIN;
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.post('/import', async (req, res) => {
+    if (!req.query.email) throw new Error('Need a query param with the email');
+    await postEmailToSubstack(req.query.email);
+    res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Simple Substack Importer listening on port ${port}!`);
+});
+
+const postEmailToSubstack = async (email) => {
+
+    // Generate hash combining your API key and the email
+    const hash = createHash('sha256').update(substack_key + email).digest('base64');
+    
+    // Set up the request
+    const reqOptions = {
+      url: `https://${subdomain}.substack.com/api/v1/import/external`,
+      method: 'POST',
+      headers: { 'Authorization': `SHA256 ${hash}` },
+      form: { email }
+    };
+    
+    await rp(reqOptions);
+  };
